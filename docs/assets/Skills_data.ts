ebadfdb927e15737a39952d8521e9853b7374e1a
Gustavz7 <guz.developer@gmail.com>
2022-08-09 21:19:39 -0400

    version de produccion

---
+++ docs/assets/Skills_data.ts
@@ -0,0 +1,26 @@
+export var skills_data = [
+  {
+    name: 'Java',
+    value: 80,
+  },
+  {
+    name: 'Javascript',
+    value: 40,
+  },
+  {
+    name: 'Typescript',
+    value: 72,
+  },
+  {
+    name: 'SQL',
+    value: 30,
+  },
+  {
+    name: 'Angular',
+    value: 50,
+  },
+  {
+    name: 'SpringBoot',
+    value: 35,
+  },
+];
