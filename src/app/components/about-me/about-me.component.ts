2c2a0f2fe6e54563f2e55a0d5cb516184b6b54a4
Guz_dev <guz.developer@gmail.com>
2024-01-09 17:25:03 -0300

    update data, some details

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+  faLinkedin = faLinkedin;
   constructor() { }
 
   ngOnInit(): void {
